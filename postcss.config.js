1aa2bf4ca2e964e549f09830099a90bb515764e8
bouzuya <m@bouzuya.net>
2019-01-26 21:45:51 +0900

    add postcss settings

---
+++ postcss.config.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const saveStyleModule = ({ srcDir, moduleNameSuffix }) => {
+  return (cssFileName, json) => {
+    const cssDir = path.dirname(cssFileName);
+    const cssName = path.basename(cssFileName, '.css');
+    const baseModuleNames = path.relative(srcDir, cssDir).split(path.sep);
+    const moduleName = cssName + moduleNameSuffix;
+    const pursFilePath = path.resolve(cssDir, moduleName + '.purs');
+    const entries = Object
+      .keys(json)
+      .sort()
+      .map((key) => ({ key, value: json[key] }));
+    const purs = [
+      'module ' + baseModuleNames.concat([moduleName]).join('.')
+    ].concat(
+      '  ( ' + entries.map((i) => i.key).join('\n  , ') + '\n  ) where'
+    ).concat(
+      entries.map(({ key, value }) => {
+        return [
+          '',
+          key + ' :: String',
+          key + ' = "' + value + '"'
+        ].join('\n');
+      })
+    ).join('\n');
+    fs.writeFileSync(pursFilePath, purs, { encoding: 'utf-8' });
+  };
+};
+
+const mySaveStyleModule = saveStyleModule({
+  srcDir: path.resolve('./src'),
+  moduleNameSuffix: 'Style'
+});
+
+module.exports = {
+  plugins: [
+    require('postcss-modules')({
+      getJSON: (cssFileName, json, _outputFileName) => {
+        return mySaveStyleModule(cssFileName, json);
+      }
+    })
+  ]
+};
